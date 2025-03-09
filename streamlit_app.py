d4f4fb729283c331e156dbdf862ab3b0234b3d83
tientien03 <ngtientien123456@gmail.com>
2025-03-09 11:41:42 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,14 @@
 import streamlit as st
+import cv2
+import numpy as np
+from PIL import Image
+import torch
 
-st.title("WELCOME TO AHTIEN CHAATTTBOAT")
-prompt = st.chat_input("Say something")
+
+st.title("Virtual Try-On")
+st.write("Capture your image using the camera:")
+person_image = st.camera_input("Take a picture")
+
 if prompt:
     st.write(f"User has sent the following prompt: {prompt}")
     
